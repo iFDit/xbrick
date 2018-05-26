33a2786d6b611f7aa196664222273fbc0666c427
iFdit <lyliyangwc@gmail.com>
2018-05-26 17:02:01 +0800

    add common modules

---
+++ src/common/classes.ts
@@ -0,0 +1,19 @@
+// types
+export const PRIMARY = 'primary'
+export const SECONDARY = 'secondary'
+export const SUCCESS = 'success'
+export const DANGER = 'danger'
+export const WARNING = 'warning'
+export const INFO = 'info'
+export const LIGHT = 'light'
+export const DARK = 'dark'
+
+// inline
+export const CLOSE = 'close'
+
+// components
+export const ALERT = 'alert'
+export const ALERT_LINK = 'alert-link'
+export const ALERT_HEADING = 'alert-heading'
+export const ALERT_DISMISSIBLE = 'alert-dismissible'
+
---
+++ src/common/props.ts
@@ -0,0 +1,5 @@
+export type AlertType = 'primary' | 'secondary' | 'success' | 'danger' | 'warning' | 'info' | 'light' | 'dark'
+
+export interface IProps {
+  [prop: string]: any
+}
