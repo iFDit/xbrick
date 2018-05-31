82c64d427648a0737d72c0e26d9f21aa6a492c82
iFdit <lyliyangwc@gmail.com>
2018-05-31 21:36:34 +0800

    add getcss util

---
+++ src/common/util.ts
@@ -0,0 +1,20 @@
+/**
+ * Get HTMLElement css value.
+ * @param dom HTMLElement
+ * @param props CSS Property name
+ * @param defaultValue 
+ * @return {any} CSS Value
+ */ 
+export function getcss(dom: HTMLElement, props: string, defaultValue: any = null) {
+  if (!dom) { return }
+  const value = getComputedStyle(dom).getPropertyValue(props)
+  const returnValue = typeof value === 'boolean' ? value : (value || defaultValue)
+
+  return typeof defaultValue === 'string' ?
+    String(returnValue)
+    : typeof defaultValue === 'number' ?
+      parseInt(returnValue, 10)
+      : typeof defaultValue === 'boolean' ?
+        !!returnValue
+        : returnValue
+}
