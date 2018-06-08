b15d67b59cb2fb1c0ab864b08727a6f336bebcee
iFdit <lyliyangwc@gmail.com>
2018-06-08 21:11:57 +0800

    modify scss and button type

--- src/common/props.ts
+++ src/common/props.ts
@@ -1,6 +1,6 @@
 export type AlertType = 'primary' | 'secondary' | 'success' | 'danger' | 'warning' | 'info' | 'light' | 'dark'
 export type BadgeType = 'primary' | 'secondary' | 'success' | 'danger' | 'warning' | 'info' | 'light' | 'dark'
-export type ButtonType = 'primary' | 'secondary' | 'success' | 'danger' | 'warning' | 'info' | 'light' | 'dark'
+export type ButtonType = 'primary' | 'secondary' | 'success' | 'danger' | 'warning' | 'info' | 'light' | 'dark' | 'link'
 export type ButtonSize = 'small' | 'large' | 'middle'
 
 export interface IProps {
