176fa26f119ec77ca61cb758590112e880245a61
iFdit <lyliyangwc@gmail.com>
2018-07-12 21:13:46 +0800

    add jumbotron component

---
+++ src/jumbotron/Jumbotron.tsx
@@ -0,0 +1,35 @@
+import * as React from 'react'
+import * as classNames from 'classnames'
+import * as classes from 'src/common/classes'
+import { IProps } from 'src/common/props'
+
+export interface IJumbotronProps extends IProps {
+  /**
+   * custom render component.
+   * @default div
+   */
+  tag?: string | React.Factory<any>
+
+  /**
+   * make Jumbotron full width.
+   * @default false
+   */
+  fluid?: boolean
+}
+
+export const Jumbotron: React.StatelessComponent<IJumbotronProps> = function (props: IJumbotronProps) {
+  const { tag, fluid, ...others } = props
+  const Tag = tag!
+  const className = classNames(
+    props.className,
+    classes.JUMBOTRON,
+    {[classes.JUMBOTRON_FLUID]: !!fluid},
+  )
+  return <Tag {...others} className={className} />
+}
+
+Jumbotron.displayName = 'xbrick.Jumbotron'
+Jumbotron.defaultProps = {
+  tag: 'div',
+  fluid: false,
+}
---
+++ src/jumbotron/__test__/Jumbotron.spec.ts
@@ -0,0 +1,58 @@
+import * as React from 'react'
+import * as sinon from 'sinon'
+import { mount } from 'enzyme'
+import { Jumbotron } from 'src/jumbotron/Jumbotron'
+
+describe('Jumbotron', () => {
+  const p = React.createFactory('p')
+  const F = React.createFactory<any>(Jumbotron)
+
+  it('should render without crash', () => {
+    mount(F())
+  })
+
+  it('should render ReactNode Children', () => {
+    const render = mount(F(null, p(null, 'button')))
+
+    expect(render.find('p').hostNodes().length).toBe(1)
+    expect(render.find('p').text()).toBe('button')
+  })
+
+  it('should render text node', () => {
+    const render = mount(F(null, 'textnode'))
+
+    expect(render.text()).toBe('textnode')
+  })
+
+  it('should pass down other props', () => {
+    const props = { className: 'test', onClick: sinon.spy() }
+    const render = mount(F(props))
+    const btn = render.find('div')
+
+    btn.simulate('click')
+    expect(btn.hasClass('test'))
+    expect(props.onClick.called).toBe(true)
+  })
+
+  it('should render with custom tag', () => {
+    const render = mount(F({ tag: 'span' }))
+
+    expect(render.find('span').hostNodes().length).toBe(1)
+  })
+
+  it('should have default props', () => {
+    const render = mount(F())
+
+    expect(render.prop('tag')).toBe('div')
+    expect(render.prop('fluid')).toBe(false)
+  })
+
+  it('should render correct className when set different props', () => {
+    const render = mount(F())
+
+    expect(render.find('div').hasClass('jumbotron')).toBe(true)
+
+    render.setProps({ fluid: true })
+    expect(render.find('div.jumbotron').hasClass('jumbotron-fluid')).toBe(true)
+  })
+})
